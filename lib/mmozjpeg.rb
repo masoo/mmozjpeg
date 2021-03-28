80eb09daa571d5869e06a6a973336064fc9e783c
FUNABARA Masao <masao@masoo.jp>
2021-03-28 01:27:45 +0000

    Implement Mmozjpeg::Cjpeg.optimize

--- lib/mmozjpeg.rb
+++ lib/mmozjpeg.rb
@@ -1,6 +1,25 @@
 require "mmozjpeg/version"
+require "pathname"
+require "open3"
 
 module Mmozjpeg
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Cjpeg
+    class << self
+      def optimize(infile:, outfile: nil, quality: nil, grayscale: nil, rgb: nil,
+        optimize: nil, progressive: nil, targa: nil, arithmetic: nil, dct: nil, icc: nil,
+        restart: nil, maxmemory: nil, memdst: nil, report: nil, verbose: nil, debug: nil,
+        version: nil, baseline: nil, qtables: nil, qslots: nil, sample: nil, scans: nil)
+
+        cjpeg = "cjpeg"
+        unless quality.nil?
+          parsed_quality = ["-quality", quality.split(",").map { |v| Integer(v) }.join(",")]
+        end
+        parsed_infile = Pathname(infile).cleanpath.to_s unless infile.nil?
+        arg = [cjpeg, parsed_quality, parsed_infile].select { |v| !v.nil? }.flatten!
+        Open3.capture2(*arg)
+      end
+    end
+  end
 end
--- spec/mmozjpeg_spec.rb
+++ spec/mmozjpeg_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe Mmozjpeg do
   end
 
   it "does something useful" do
-    expect(false).to eq(true)
+    _, s = Mmozjpeg::Cjpeg.optimize(infile: "./spec/test.jpg", outfile: nil, quality: "50,80", grayscale: true, rgb: true,
+                                    optimize: true, progressive: true, targa: true, arithmetic: true, dct: "int/fast/float", icc: "/to/path",
+                                    restart: 0, maxmemory: 0, memdst: true, report: true, verbose: true, debug: true, version: true,
+                                    baseline: true, qtables: "/to/path", qslots: "50,80s", sample: "2x2,4x4", scans: "/to/path")
+
+    expect(s.exitstatus).to eq(0)
   end
 end
