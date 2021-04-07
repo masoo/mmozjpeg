cbf016c275c46194ce306a989efa5400254655ec
FUNABARA Masao <masao@masoo.jp>
2021-04-07 05:23:22 +0000

    add supported? method.

--- lib/mmozjpeg.rb
+++ lib/mmozjpeg.rb
@@ -7,6 +7,17 @@ module Mmozjpeg
 
   class Cjpeg
     class << self
+      # Whether cjpeg exists or not?
+      # @return [True] "cjpeg" exists,
+      #         [False] "cjpeg" NOT exists
+      def supported?
+        args = ["cjpeg", "-version"]
+        _, s = Open3.capture2(*args)
+        s
+      rescue Errno::ENOENT
+        false
+      end
+
       def optimize(infile:, outfile: nil, quality: nil, grayscale: nil, rgb: nil,
         optimize: nil, progressive: nil, targa: nil, arithmetic: nil, dct: nil, icc: nil,
         restart: nil, maxmemory: nil, memdst: nil, report: nil, verbose: nil, debug: nil,
@@ -17,8 +28,8 @@ module Mmozjpeg
           parsed_quality = ["-quality", quality.split(",").map { |v| Integer(v) }.join(",")]
         end
         parsed_infile = Pathname(infile).cleanpath.to_s unless infile.nil?
-        arg = [cjpeg, parsed_quality, parsed_infile].select { |v| !v.nil? }.flatten!
-        Open3.capture2(*arg)
+        args = [cjpeg, parsed_quality, parsed_infile].select { |v| !v.nil? }.flatten!
+        Open3.capture2(*args)
       end
     end
   end
--- spec/mmozjpeg_spec.rb
+++ spec/mmozjpeg_spec.rb
@@ -3,7 +3,11 @@ RSpec.describe Mmozjpeg do
     expect(Mmozjpeg::VERSION).not_to be nil
   end
 
-  it "does something useful" do
+  it "#supported?" do
+    expect(Mmozjpeg::Cjpeg.supported?).to be_truthy
+  end
+
+  it "#optimize" do
     _, s = Mmozjpeg::Cjpeg.optimize(infile: "./spec/test.jpg", outfile: nil, quality: "50,80", grayscale: true, rgb: true,
                                     optimize: true, progressive: true, targa: true, arithmetic: true, dct: "int/fast/float", icc: "/to/path",
                                     restart: 0, maxmemory: 0, memdst: true, report: true, verbose: true, debug: true, version: true,
